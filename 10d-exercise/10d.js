7b8731c59208e4dba005993a4950e6a62e3f4b6d
Sameera Rajapakshe <svirajrajapaksha18@gmail.com>
2023-05-07 12:42:28 +0530

    Update 10d exercise

--- 10d-exercise/10d.js
+++ 10d-exercise/10d.js
@@ -1,10 +1,23 @@
 function clickButton(selecter){
   const btnElement = document.querySelector(selecter);
 
+  //remove previous button
+
+  turnOfPreviousButton();
+
   if (!btnElement.classList.contains('is-toggled')){
     btnElement.classList.add('is-toggled');
   }else {
     btnElement.classList.remove('is-toggled');
+  }
+}
+
+function turnOfPreviousButton(){
+  const previousButton = document.querySelector('.is-toggled');
+
+
+  if (previousButton) {
+    previousButton.classList.remove('is-toggled');
   }
 }
 
