ed68ff98d8cab53fe00bdd94ca85beae7eabbf7d
Sameera Rajapakshe <svirajrajapaksha18@gmail.com>
2023-05-10 16:10:47 +0530

    update todo list

--- Todo list/Todo-list.js
+++ Todo list/Todo-list.js
@@ -1,4 +1,10 @@
-const todoList = [];
+const todoList = [{
+  name:'make dinner',
+  dueDate: '2022-12-22'
+},{
+  name:'make dinner',
+  dueDate: '2022-12-22'
+}];
 
 renderTodo();
 
@@ -6,8 +12,25 @@ function renderTodo(){
   let todoListHTML ='';
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`; //generating html using js
+    const todoObj = todoList[i];
+    //const name = todoObj.name;
+    //const dueDate = todoObj.dueDate;
+
+    //shortcut
+
+    const { name } = todoObj;
+    const { dueDate } = todoObj;
+
+    //delete button
+
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick = "
+      todoList.splice(${i},1);
+      renderTodo();      
+    " class ="btn-dlt">Delete</button>
+    </div>`; //generating html using js
     todoListHTML+=html;
   }
   
@@ -19,9 +42,15 @@ function addTodo(){
   const inputElement = document.querySelector('.js-name-input');
 
   const name = inputElement.value;
+
+  const dateElement = document.querySelector('.js-date-input');
+
+  const dueDate = dateElement.value;
   
-  todoList.push(name);
-  console.log(todoList);
+  todoList.push({
+    name: name,
+    dueDate: dueDate
+  });
 
   //reset textbox
 
